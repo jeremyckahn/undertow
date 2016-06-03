0b19ca11e2985cfa0a4ac01ac751cce093296a12
Jeremy Kahn <jeremyckahn@gmail.com>
2016-06-03 08:15:45 -0500

    Serve Stylie and Mantra

--- app.js
+++ app.js
@@ -19,7 +19,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/stylie',
+  express.static(path.join(__dirname, 'node_modules/stylie'))
+);
+
+app.use('/mantra',
+  express.static(path.join(__dirname, 'node_modules/mantra'))
+);
 
 app.use('/', routes);
 
