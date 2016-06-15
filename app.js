40fa7d73954f0bfaf1dba4d36c2ec51bfe51c62f
Jeremy Kahn <jeremyckahn@gmail.com>
2016-06-14 20:47:38 -0500

    Swtich to rekapi.com repo for Rekapi static site files

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/',
-  express.static(path.join(__dirname, 'node_modules/rekapi'))
+  express.static(path.join(__dirname, 'node_modules/rekapi.com'))
 );
 
 var stylieRoutes = require('./routes/stylie');
