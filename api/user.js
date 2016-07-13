4bbcc81826ba990248e00065d726d75fe0eee9fe
Jeremy Kahn <jeremyckahn@gmail.com>
2016-07-13 08:16:57 -0500

    Convert /user/:method to POST

--- api/user.js
+++ api/user.js
@@ -8,14 +8,13 @@ const app = require('../app');
 const { dataAdapter } = app;
 
 function createUser(req, res, next) {
-  // FIXME: Switch away from .query once this is no longer a GET route handler
-  const { name, password } = req.query;
+  const { name, password } = req.body;
   User.create({ name, password, dataAdapter })
     .then(u => res.send(u))
     .catch(e => res.send(e));
 }
 
-router.get('/user/:method', function (req, res, next) {
+router.post('/user/:method', function (req, res, next) {
   const { method } = req.params;
 
   switch (method) {
